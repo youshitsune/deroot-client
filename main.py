cce3e79213c019c223e6a27523dc9bee3fe02333
youshitsune <youshitsune@dmz.rs>
2023-07-28 07:15:55 +0200

    implement back and error for not found files

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import threading
 import socket
+import os
 
 HEADER = 64
 target_host = "127.0.0.1"
@@ -8,18 +9,32 @@ target_port = 5050
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((target_host, target_port))
 
+client.send(str(len(b"index")).encode()+b' ' * (HEADER - len(str(len(b"index")).encode())))
+client.send(b"index")
+size = client.recv(HEADER).decode()
+index = client.recv(int(size)).decode()
+
 def run(client):
-    client.send(str(len(b"index")).encode()+b' ' * (HEADER - len(str(len(b"index")).encode())))
-    client.send(b"index")
-    size = client.recv(HEADER).decode()
-    index = client.recv(int(size)).decode()
     print(index)
     while True:
         req = int(input(f"Select file [0-{len(index.splitlines())-1}]: "))
+        os.system("clear")
         client.send(str(len(index.splitlines()[req].encode())).encode()+b' ' * (HEADER - len(str(len(index.splitlines()[req].encode())).encode())))
         client.send(index.splitlines()[req].encode())
         size = client.recv(HEADER).decode()
         if size:
-            print(client.recv(int(size)).decode())
+            res = client.recv(int(size)).decode()
+            if res != "404":
+                print(res)
+            else:
+                print("404 File Not Found")
+                back =  input("Get back[b]: ")
+                if back == "b":
+                    os.system("clear")
+                    run(client)
+        back = input("Get back[b]: ")
+        if back == "b":
+            os.system("clear")
+            run(client)
 
 run(client)
